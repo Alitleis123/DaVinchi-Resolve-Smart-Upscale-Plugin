a98e028b4d75aaf4c1c986961ab0f53d56851f0d
Alitleis123 <alitleis0731@gmail.com>
2026-02-09 19:18:59 -0500

    testing

--- Pipeline/__init__.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class upscaleConfig:
-    upscale_enabled: bool = False
-    interprolate_enabled: bool = False
-
-    upscale_factor: int = 2
-    interprolate_factor: int = 2
